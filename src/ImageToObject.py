6b1e6c6002fccd552a2d3697466e164db2f26ec1
agniaendie <endienasg3@gmail.com>
2024-06-16 02:09:35 +0500

    fix cluster

--- src/ImageToObject.py
+++ src/ImageToObject.py
@@ -1,8 +1,11 @@
 import requests
 import cv2
 from flask import *
+import os
 
 app = Flask(__name__)
+os.environ["OPENCV_VIDEOIO_DEBUG"] = 1
+os.environ["OPENCV_LOG_LEVEL"] = "debug"
 
 
 def neural(image):
@@ -16,12 +19,13 @@ def neural(image):
 @app.route("/video/<id>")
 def video(id):
     address = f"http://image-resize-service/resize/{id}"
-    localCamera = cv2.VideoCapture(address)
+    localCamera = cv2.VideoCapture(address, cv2.CAP_V4L)
     success, frame = localCamera.read()
     if success:
-        #ret, jpeg = cv2.imencode('.jpg', frame)
+        # ret, jpeg = cv2.imencode('.jpg', frame)
         return neural(frame)
     else:
         return "none"
+
 
 app.run("0.0.0.0", port=8082, debug=True)
