e0f6b6a96f4d63add4c5b625c220eff63b38af0b
agniaendie <endienasg3@gmail.com>
2024-06-15 21:42:50 +0500

    fix camera

--- src/ImageToObject.py
+++ src/ImageToObject.py
@@ -1,18 +1,21 @@
 import requests
+import cv2
 from flask import *
 
 app = Flask(__name__)
 
 
 def neural(image):
-    #test
-    return image
+    # test
+    return Response(image, mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route("/video/<id>")
 def video(id):
-    image = requests.get(f"http://image-resize-service/resize/{id}")
-    return neural(image)
+    localCamera = cv2.VideoCapture(f"http://image-resize-service/resize/{id}")
+    success, frame = localCamera.read()
+
+    return neural(frame)
 
 
 app.run("0.0.0.0", port=8082, debug=True)
