842425f6d59950b5c12da7f85362e680f2a0cc84
agniaendie <endienasg3@gmail.com>
2024-06-15 23:14:31 +0500

    fix cluster

--- src/ImageToObject.py
+++ src/ImageToObject.py
@@ -7,15 +7,18 @@ app = Flask(__name__)
 
 def neural(image):
     # test
-    return Response(image, mimetype='multipart/x-mixed-replace; boundary=frame')
+    # convert
+
+    # return Response(image, mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(image, mimetype='image/jpeg')
 
 
 @app.route("/video/<id>")
 def video(id):
     localCamera = cv2.VideoCapture(f"http://image-resize-service/resize/{id}")
     success, frame = localCamera.read()
-
-    return neural(frame)
+    ret, jpeg = cv2.imencode('.jpg', frame)
+    return neural(jpeg)
 
 
 app.run("0.0.0.0", port=8082, debug=True)
